a31995454708d20253890176facd4cd478b1bd4e
Mika <mikahurvits@mail.tau.ac.il>
2024-09-13 19:28:26 +0300

    added more components on the upload page

---
+++ server/routes/Upload.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const Recipe = require('../models/RecipeModel');
+const e = require('express');
+
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const { title, categories, imageUrl, ingredients, totalTime, instructions, createdBy } = req.body;
+
+    try{
+        const recipe = await Recipe.create({ title, categories, imageUrl, ingredients, totalTime, instructions, createdBy });
+        res.status(200).json(recipe);
+
+
+    }
+    catch(error){
+        res.status(400).json({error: error.message})
+
+    }
+    res.json({mssg: 'upload successful'})
+    console.log(req.body)
+    res.send('upload successful')
+}
+)
+
+
+module.exports = router;
